018cdea1116d54ce98cbebe5ff62c9cfdc92b018
Dawid Łęczycki <leczyckidawid13@gmail.com>
2024-12-06 23:56:36 +0100

    initial commit
    started coding chrome extension

---
+++ index.js
@@ -0,0 +1,13 @@
+let inputBtn = document.getElementById("input-btn")
+let boxEl = document.getElementById("box")
+
+inputBtn.addEventListener("click", function(){
+    console.log("click clack bang pow ding bash");
+    
+})
+
+
+boxEl.addEventListener("click", function(){
+    console.log("aint no way ");
+    
+})
